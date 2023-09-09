efa2198af551dc25658385b2a842b78da612fa0e
EC2 Default User <ec2-user@ip-172-31-34-75.ec2.internal>
2023-09-09 12:20:17 +0000

    deleted

--- file3.sh
+++ file3.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 echo "enter age"
 read age
-if [ $age -le 13 ]
+if [ $age -le 13 ];
 then 
 	echo "child"
-elif [ $age -gt 13  && $age -eq 19 ]
+elif ([ $age -gt 13  && $age -eq 19 ])
 then 
-       echo "teenage"
+	echo "teenage"
 elif [ $age -gt 20 ]
 then 
-       echo "adult"
+	echo "adult"
 else
-       echo "invaild"
+	echo "invaild"
 fi
+
 
   
 
